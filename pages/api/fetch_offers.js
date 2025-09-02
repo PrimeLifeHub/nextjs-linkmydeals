2769b96d58db0cf6b76c77468216052ab0888831
PrimeLifeHub <best.prime.core@gmail.com>
2025-09-02 18:21:08 -0400

    Update fetch_offers.js

--- pages/api/fetch_offers.js
+++ pages/api/fetch_offers.js
@@ -1,6 +1,3 @@
-
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   const api_key = '7ef866099caa3b69cc46c9d44a7f8284';
 
@@ -8,7 +5,6 @@ export default async function handler(req, res) {
     const response = await fetch(`https://feed.linkmydeals.com/getOffers/?API_KEY=${api_key}&format=json`);
     const data = await response.json();
 
-    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ Result: false, error: error.message });
